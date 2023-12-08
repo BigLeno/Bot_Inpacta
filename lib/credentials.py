cda7924b01ff602bb86b3e9102e5fc372f2a2c1b
Rutileno Gabriel <rutileno.silva.702@ufrn.edu.br>
2023-12-07 23:19:40 -0300

    adicionando o acesso as credenciais do google

--- lib/credentials.py
+++ lib/credentials.py
@@ -15,9 +15,29 @@ def get_credentials() -> Tuple[str, str, str]:
     admin = getenv('ADMIN')
 
     if not token or not name_bot or not admin:
-        warning("Uma ou mais variáveis de ambiente estão faltando.")
+        warning("Uma ou mais variáveis de ambiente do telegram estão faltando.")
         return False
     else:
-        info("Variáveis de ambiente carregadas com sucesso.")
+        info("Variáveis de ambiente do telegram carregadas com sucesso.")
 
     return token, name_bot, admin
+
+def get_google_credentials() -> Tuple[str]:
+    """
+        Função para pegar o diretório do client_secret
+    """
+    basicConfig(level=INFO)
+    load_dotenv()
+    client_directory = getenv('CLIENT_DIRECTORY')
+    token_directory = getenv('TOKEN_DIRECTORY')
+    scopes = getenv('SCOPES')
+    sample_spreadsheet_id = getenv('SAMPLE_SPREADSHEET_ID')
+    sample_range_name = getenv('SAMPLE_RANGE_NAME')
+
+    if not client_directory or not token_directory or not scopes or not sample_spreadsheet_id or not sample_range_name:
+        warning("Uma ou mais variáveis de ambiente do google estão faltando.")
+        return False
+    else:
+        info("Variáveis de ambiente do google carregadas com sucesso.")
+
+    return client_directory, token_directory, scopes, sample_spreadsheet_id, sample_range_name
