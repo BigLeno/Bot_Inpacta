3bb2cc2e4e364126fb88c22fb8e2e46738394102
Rutileno Gabriel <rutileno.silva.702@ufrn.edu.br>
2023-12-09 14:38:27 -0300

    novas implementações

--- app/modules/lib/credentials.py
+++ app/modules/lib/credentials.py
@@ -22,6 +22,48 @@ def get_credentials() -> Tuple[str, str, str]:
 
     return token, name_bot, admin
 
+def get_users_id() -> Tuple[str, str, str, str, str, str]:
+    """
+        Função para pegar as credenciais do .env
+    """
+    basicConfig(level=INFO)
+    load_dotenv()
+    user1 = getenv('USER_ID_1')
+    user2 = getenv('USER_ID_2')
+    user3 = getenv('USER_ID_3')
+    user4 = getenv('USER_ID_4')
+    user5 = getenv('USER_ID_5')
+    user6 = getenv('USER_ID_6')
+
+    if not user1 or not user2 or not user3 or not user4 or not user5 or not user6:
+        warning("Uma ou mais variáveis de ambiente de id dos usuários estão faltando.")
+        return False
+    else:
+        info("Variáveis de ambiente de id dos usuários carregadas com sucesso.")
+
+    return user1, user2, user3, user4, user5, user6
+
+def get_users_names() -> Tuple[str, str, str, str, str, str]:
+    """
+        Função para pegar as credenciais do .env
+    """
+    basicConfig(level=INFO)
+    load_dotenv()
+    username1 = getenv('USER_NAME_1')
+    username2 = getenv('USER_NAME_2')
+    username3 = getenv('USER_NAME_3')
+    username4 = getenv('USER_NAME_4')
+    username5 = getenv('USER_NAME_5')
+    username6 = getenv('USER_NAME_6')
+
+    if not username1 or not username2 or not username3 or not username4 or not username5 or not username6:
+        warning("Uma ou mais variáveis de ambiente de nome dos usuários estão faltando.")
+        return False
+    else:
+        info("Variáveis de ambiente de nome dos usuários carregadas com sucesso.")
+
+    return username1, username2, username3, username4, username5, username6
+
 def get_google_credentials() -> Tuple[str]:
     """
         Função para pegar o diretório do client_secret
