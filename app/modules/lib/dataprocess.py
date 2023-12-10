4909679a0948c5f156512101374a4412a11ee2ca
Rutileno Gabriel <rutileno.silva.702@ufrn.edu.br>
2023-12-10 16:16:38 -0300

    Processamento de dados quase definitivo

--- app/modules/lib/dataprocess.py
+++ app/modules/lib/dataprocess.py
@@ -7,12 +7,11 @@ from modules.lib.googleservices import get_sheets
 # Para rodar localmente
 # from googleservices import get_sheets
 
-
 # Definindo o nível do log
 basicConfig(level=INFO)
 
 # Mapeia dias da semana para índices
-day_to_index = {'Segunda': 1, 'Terça': 2, 'Quarta': 3, 'Quinta': 4, 'Sexta': 5}
+day_to_index = { 0 :'Segunda', 1:'Terça', 2:'Quarta', 3:'Quinta', 4:'Sexta', 5:'Sábado', 6:'Domingo'}
 
 # Lista de horários permitidos
 allowed_times = ['M12', 'M34', 'M56', 'T12', 'T34', 'T56', 'N12', 'N34']
@@ -29,38 +28,43 @@ time_to_interval = {
     'N34': range(21, 22)  # 22:05-23:55
 }
 
-def get_day(day):
-    if day.lower() == 'hoje':
-        return (datetime.now().weekday())
-    elif day.lower() == 'ontem':
-        return (datetime.now() - timedelta(days=1)).weekday()
-    elif day.lower() == 'amanhã':
-        return (datetime.now() + timedelta(days=1)).weekday()
-    else:
-        return day_to_index.get((day.lower()).capitalize(), -1)
+def get_day(user_input:str) -> int or None:
+    """Retorna o dia da semana correspondente ao input do usuário."""
+    if user_input.lower() == "hoje":
+        return datetime.now().date().weekday()
+    elif user_input.lower() == "amanhã":
+        return (datetime.now().date() + timedelta(days=1)).weekday()
+
+    try:
+        if user_input[-4:].isdigit():
+            return datetime.strptime(user_input, '%d/%m/%Y').date()
+        else:
+            return datetime.strptime(user_input + '/' + str(datetime.now().year), '%d/%m/%Y').date().weekday()
+
+    except ValueError:
+        print("Não foi possível entender a data. Por favor, use o formato DD/MM ou DD/MM/AAAA.")
+        return None
+
     
-def get_time_interval(time):
+def get_time_interval(time) -> str:
+    """Retorna o intervalo de tempo correspondente ao input do usuário."""
+
     if time in allowed_times:
         return time
     else:
         if time.isdigit():
-            time = time + ':00'
-        elif 'h' in time:
-            if '00' not in time and ':' not in time:  # Se não houver minutos, adiciona ':00'
-                time = time.replace('h', ':00')
-            else: # Se houver minutos, adiciona ':'
-                time = time.replace('h', ':')
-        time = datetime.strptime(time, '%H:%M').time()
-        hour = time.hour
-        minute = time.minute
+            time += ':00'
+        elif 'h' in time or 'H' in time:
+            time = time.replace('h', ':').replace('H', ':')
+            if len(time) == 3:  # Se não houver minutos, adiciona ':00'
+                time += '00'
+
+        hour, minute = datetime.strptime(time, '%H:%M').time().hour, datetime.strptime(time, '%H:%M').time().minute
+
         for interval, hours in time_to_interval.items():
-            if hour in hours and minute < 50:  # Considera apenas os primeiros 50 minutos de cada hora
-                return interval
-            elif hour in hours and minute >= 50:  # Se os minutos estão próximos do final do intervalo
-                next_interval = next((i for i in time_to_interval if i > interval), None)  # Encontra o próximo intervalo
-                if next_interval:
-                    return next_interval
-    # Se a hora não corresponder a nenhum intervalo, retorna o intervalo mais próximo
+            if hour in hours:
+                return interval if minute < 50 else next((i for i in time_to_interval if i > interval), None)
+                
     return min(time_to_interval.keys(), key=lambda interval: abs(hour - time_to_interval[interval][0]))
 
 def get_data_from_sheets(day, time):
@@ -68,14 +72,13 @@ def get_data_from_sheets(day, time):
     result = get_sheets()
 
     if not result:
-      return False
+      return "Não foi possível acessar a planilha. Tente novamente mais tarde."
 
-    day_index = get_day(day)
+    day_index = get_day(day) + 1
     formated_time = get_time_interval(time)
-    info(f"Horário: {formated_time} - Dia: {day}")
 
     for row in result:
-      if all(item not in row for item in ['M12','N34','DIAS DA SEMANA']) and row:
+        if all(item not in row for item in ['M12','N34','DIAS DA SEMANA']) and row and day_index not in [6, 7]:
           if (row[0] == formated_time and 
             0 < day_index < len(row) and 
             formated_time in allowed_times and 
@@ -103,11 +106,15 @@ def get_data_from_sheets(day, time):
           
           elif formated_time not in allowed_times and 0 < day_index < len(row):
             warning("Horário inválido.")
-            return False
+            return "Horário inválido."
           
           elif not (0 < day_index < len(row)):
             warning("Dia inválido.")
-            return False
+            return "Dia inválido."
+          
+        elif day_index in [6, 7]:
+            warning("Final de semana, não tem horário.")
+            return "Final de semana, não tem horário."
           
 
 # if __name__ == "__main__":
