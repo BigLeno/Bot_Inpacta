442f9a4e1278c5762377884c97f5545724f48038
Rutileno Gabriel <rutileno.silva.702@ufrn.edu.br>
2023-12-12 03:22:37 -0300

    Objetificando Validation

--- app/modules/lib/validation.py
+++ app/modules/lib/validation.py
@@ -1,24 +1,29 @@
 from time import strptime
 
-def is_valid_date(date_str) -> bool:
-    """Verifica se a data está no formato dd/mm."""
+class Validation:
+    """Classe para validação de dados."""
 
-    if date_str in ['hoje', 'amanhã', 'amanha']:
-        return True
+    @classmethod
+    def is_valid_date(cls, date_str) -> bool:
+        """Verifica se a data está no formato dd/mm."""
 
-    try:
-        strptime(date_str, '%d/%m')
-        return True
-    except ValueError:
-        return False
+        if date_str in ['hoje', 'amanhã', 'amanha']:
+            return True
 
-def is_valid_time(time_str) -> bool:
-    """Verifica se o horário está no formato hh:mm."""
-    formats = ['%H:%M', '%Hh', '%Hh%M', '%HH', '%HH%M']
-    for fmt in formats:
         try:
-            strptime(time_str, fmt)
+            strptime(date_str, '%d/%m')
             return True
         except ValueError:
-            continue
-    return False
+            return False
+
+    @classmethod
+    def is_valid_time(cls, time_str) -> bool:
+        """Verifica se o horário está no formato hh:mm."""
+        formats = ['%H:%M', '%Hh', '%Hh%M', '%HH', '%HH%M']
+        for fmt in formats:
+            try:
+                strptime(time_str, fmt)
+                return True
+            except ValueError:
+                continue
+        return False
