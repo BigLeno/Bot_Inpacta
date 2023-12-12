91fae4dbb2bbf90ea56ae8c6cbd0d5fd8c2e1f30
Rutileno Gabriel <rutileno.silva.702@ufrn.edu.br>
2023-12-12 05:48:06 -0300

    retirando os 'get'

--- app/modules/lib/messages.py
+++ app/modules/lib/messages.py
@@ -9,7 +9,7 @@ from modules.lib.jsonutils import JsonUtils
 class MessageData:
 
     @classmethod
-    def get_data(cls, message:object, token:str, time_sleep:int or float, admin:list)-> None:
+    def get_data(cls, message:object, bot, time_sleep:int or float, admin:list)-> None:
         """Função para carregar os dados do usuário."""
         chatid = message.chat.id
         usuario = message.from_user.username
@@ -19,10 +19,10 @@ class MessageData:
         info("Dados do usuário carregados.")
         mensagem = f"""ChatID: {chatid}\nUsuário: {usuario}\nNome e sobrenome : {nome} {sobrenome}\nMensagem: {text}"""
         sleep(time_sleep)
-        get(f'https://api.telegram.org/bot{token}/sendmessage?chat_id={admin[0].id}&text={mensagem}')
+        bot.send_message(admin[0].id, mensagem)
 
     @classmethod
-    def manage_delivery(cls, data:tuple, user_data:dict, time_sleep:float or int, cachedirectory:str, user_names:list, user_ids:list, token:str) -> None:
+    def manage_delivery(cls, data:tuple, user_data:dict, time_sleep:float or int, cachedirectory:str, user_names:list, user_ids:list, bot) -> None:
         """Função para gerenciar a entrega da mensagem."""
         try:
             identificador = str(uuid4())
@@ -36,4 +36,4 @@ class MessageData:
                 recipient = user_ids[user_names.index(name)]
                 msg = f'Olá {name}, {user_data["name"]} quer agendar um horário com você no dia {user_data["day_and_time"]}. \nConteúdo: {user_data["content"]}'
                 sleep(time_sleep)
-                get(f'https://api.telegram.org/bot{token}/sendmessage?chat_id={recipient}&text={msg}')
+                bot.send_message(recipient, msg)
