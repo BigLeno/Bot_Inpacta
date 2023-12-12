6a4d967fe82c57513afd15fdc47526d496984b5e
Rutileno Gabriel <rutileno.silva.702@ufrn.edu.br>
2023-12-12 03:08:05 -0300

    Objetificando JsonUtils

--- app/modules/lib/jsonutils.py
+++ app/modules/lib/jsonutils.py
@@ -2,40 +2,44 @@
 from json import load, dump
 from logging import warning
 
-def write_json(data, identificador, filename) -> str:
-    """Função para escrever no arquivo json."""
+class JsonUtils:
 
-    try:
-        with open(filename, 'r') as file:
-            file_data = load(file)
-    except FileNotFoundError:
-        warning("Não encontrei o arquivo 'cache.json'.")
-        file_data = {}
+    @classmethod
+    def write_json(cls, data, identificador, filename) -> str:
+        """Função para escrever no arquivo json."""
 
-    file_data[identificador] = data
+        try:
+            with open(filename, 'r') as file:
+                file_data = load(file)
+        except FileNotFoundError:
+            warning("Não encontrei o arquivo 'cache.json'.")
+            file_data = {}
 
-    try:
-        with open(filename, 'w') as file:
-            dump(file_data, file)
-    except FileNotFoundError:
-        warning("Não encontrei o arquivo 'cache.json'.")
-        return "Não encontrei o arquivo json."
+        file_data[identificador] = data
 
-def read_and_remove_first_item(filename) -> tuple or str:
-    """Função para ler e remover o primeiro item do arquivo json."""
-    try:
-        with open(filename, 'r+') as file:
-            data = load(file)
-            items = list(data.items())
-            if len(items) == 0:
-                return "Não há mensagens para serem enviadas."
-            first_item = items.pop(0)
-            data = dict(items)
-            file.seek(0)  # Move o cursor para o início do arquivo
-            dump(data, file)
-            file.truncate()  # Remove o restante do conteúdo do arquivo
-    except FileNotFoundError:
-        warning("Não encontrei o arquivo 'cache.json'.")
-        return "Não encontrei o arquivo json."
+        try:
+            with open(filename, 'w') as file:
+                dump(file_data, file)
+        except FileNotFoundError:
+            warning("Não encontrei o arquivo 'cache.json'.")
+            return "Não encontrei o arquivo json."
 
-    return first_item
+    @classmethod
+    def read_and_remove_first_item(cls, filename) -> tuple or str:
+        """Função para ler e remover o primeiro item do arquivo json."""
+        try:
+            with open(filename, 'r+') as file:
+                data = load(file)
+                items = list(data.items())
+                if len(items) == 0:
+                    return "Não há mensagens para serem enviadas."
+                first_item = items.pop(0)
+                data = dict(items)
+                file.seek(0)  # Move o cursor para o início do arquivo
+                dump(data, file)
+                file.truncate()  # Remove o restante do conteúdo do arquivo
+        except FileNotFoundError:
+            warning("Não encontrei o arquivo 'cache.json'.")
+            return "Não encontrei o arquivo json."
+
+        return first_item
