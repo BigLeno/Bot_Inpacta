e585e862aac1355d99036830ac8a8e1af909a129
Rutileno Gabriel <rutileno.silva.702@ufrn.edu.br>
2023-12-11 21:04:01 -0300

    modificando e simplificando o código

--- app/app.py
+++ app/app.py
@@ -1,18 +1,23 @@
 
 from logging import basicConfig, warning, info, INFO
-from telebot import TeleBot
-from requests import get
-from uuid import uuid4
-from time import strptime, sleep
+from dotenv import load_dotenv
 
-from modules.lib.credentials import get_credentials
-from modules.lib.dataprocess import get_data_from_sheets
-from modules.lib.users import Users
-from modules.lib.jsonutils import write_json, read_and_remove_first_item
-
+# Carregando as credenciais do .env
+load_dotenv()
 
 # Definindo o nível do log
 basicConfig(level=INFO)
+
+from telebot import TeleBot
+from requests import get
+from time import sleep
+
+from modules.lib.jsonutils import read_and_remove_first_item
+from modules.lib.validation import is_valid_date, is_valid_time
+from modules.lib.messages import get_data, manage_delivery
+from modules.lib.dataprocess import get_data_from_sheets
+from modules.lib.credentials import get_credentials
+from modules.lib.users import Users
 
 token, name_bot, admin, absolutepath, cachedirectory = get_credentials() 
 
@@ -30,43 +35,9 @@ get(f'https://api.telegram.org/bot{token}/sendmessage?chat_id={admin[0].id}&text
 sleep(time_sleep)
 
 
-def is_valid_date(date_str) -> bool:
-    """Verifica se a data está no formato dd/mm."""
-
-    if date_str in ['hoje', 'amanhã', 'amanha']:
-        return True
-
-    try:
-        strptime(date_str, '%d/%m')
-        return True
-    except ValueError:
-        return False
-
-def is_valid_time(time_str) -> bool:
-    """Verifica se o horário está no formato hh:mm."""
-    formats = ['%H:%M', '%Hh', '%Hh%M', '%HH', '%HH%M']
-    for fmt in formats:
-        try:
-            strptime(time_str, fmt)
-            return True
-        except ValueError:
-            continue
-    return False
-    
-def get_data(message):
-    chatid = message.chat.id
-    usuario = message.from_user.username
-    nome = message.from_user.first_name
-    sobrenome = message.from_user.last_name
-    text = message.text
-    info("Dados do usuário carregados.")
-    mensagem = f"""ChatID: {chatid}\nUsuário: {usuario}\nNome e sobrenome : {nome} {sobrenome}\nMensagem: {text}"""
-    sleep(time_sleep)
-    get(f'https://api.telegram.org/bot{token}/sendmessage?chat_id={admin[0].id}&text={mensagem}')
-
 @bot.message_handler(commands=['start'])
 def send_welcome(message) -> None:
-    get_data(message)
+    get_data(message, token, time_sleep, admin)
     data = """Olá, aqui é o Bot da Inpacta, para ver os comandos disponíveis digite:\n  /Ajuda """
     sleep(time_sleep)
     bot.reply_to(message, data)
@@ -74,7 +45,7 @@ def send_welcome(message) -> None:
 @bot.message_handler(func=lambda message: "sim" in message.text.lower() or "não" in message.text.lower() or "nao" in message.text.lower())
 def handle_specific_chats(message) -> None:
     """Função para gerenciar as mensagens específicas."""
-    get_data(message)
+    get_data(message, token, time_sleep, admin)
     if "sim" in message.text.lower():
         msg = "Ok, aguarde um momento..."
         bot.reply_to(message, msg)
@@ -103,7 +74,7 @@ def handle_specific_chats(message) -> None:
 
 @bot.message_handler(func=lambda message: "ajuda" in message.text.lower())
 def send_help(message):
-    get_data(message)
+    get_data(message, token, time_sleep, admin)
     data = """
     Comandos disponíveis:
     /horarios - Exibe os horários
@@ -117,19 +88,19 @@ def send_help(message):
 
 @bot.message_handler(commands=['sobre'])
 def send_sobre(message):
-    get_data(message)
+    get_data(message, token, time_sleep, admin)
     data = """Bot em desenvolvimento pela Inpacta, para mais informações acesse:\n    https://sites.google.com/view/inpacta/"""
     bot.reply_to(message, data)
 
 @bot.message_handler(commands=['agendar'])
 def send_agendar(message):
-    get_data(message)
+    get_data(message, token, time_sleep, admin)
     data = """Para agendar um horário, entre com: \n'agendar data horário conteúdo' \n   Exemplo: agendar 01/01 10:00 "encontro com o gestor" """
     bot.reply_to(message, data)
 
 @bot.message_handler(func=lambda message: "agendar" in message.text.lower())
 def handle_schedule(message):
-    get_data(message)
+    get_data(message, token, time_sleep, admin)
     args = message.text.split()
     print(f" agendar args: {args}")
     if args[0].lower() != "agendar":
@@ -160,45 +131,28 @@ def handle_schedule(message):
             name = message.from_user.first_name + ' ' + message.from_user.last_name
             user_data = {'recipient': recipient, 'name': name, 'content': content, 'day_and_time': f'{day} {time}'}
             response = f'Entrei em contato com o(s) bolsista(s) e aguardando a resposta.'
-            manage_delivery(data, user_data)
+            manage_delivery(data, user_data, time_sleep, cachedirectory, user_names, user_ids, token)
             sleep(time_sleep)
             bot.reply_to(message, response)
 
 
-def manage_delivery( data:tuple, user_data:dict):
-    """Função para gerenciar a entrega da mensagem."""
-    try:
-        identificador = str(uuid4())
-        write_json(user_data, identificador, cachedirectory)
-    except Exception as err:
-        warning(f"Não foi possível escrever no arquivo json. {err}")
-        return
-    
-    for name in user_names:
-        if name in data:
-            recipient = user_ids[user_names.index(name)]
-            msg = f'Olá {name}, {user_data["name"]} quer agendar um horário com você no dia {user_data["day_and_time"]}. \nConteúdo: {user_data["content"]}'
-            sleep(time_sleep)
-            get(f'https://api.telegram.org/bot{token}/sendmessage?chat_id={recipient}&text={msg}')
-
-
 @bot.message_handler(commands=['gestores'])
 def send_gestores(message):
-    get_data(message)
+    get_data(message, token, time_sleep, admin)
     data = """
     Em desenvolvimento..."""
     bot.reply_to(message, data)
 
 @bot.message_handler(commands=['bolsistas'])
 def send_bolsistas(message):
-    get_data(message)
+    get_data(message, token, time_sleep, admin)
     data = """
     Em desenvolvimento..."""
     bot.reply_to(message, data)
 
 @bot.message_handler(commands=['horarios'])
 def send_horarios(message):
-    get_data(message)
+    get_data(message, token, time_sleep, admin)
     data = """
     Os horários disponíveis são: \n   > matutino \n   > vespertino \n   > noturno \nExemplo: \n     "horarios matutino" \nExibe o horário do
     "horario matutino" """
@@ -212,7 +166,7 @@ def send_horarios(message):
                      and "/" not in message.text.lower()
                      )
 def send_horarios_matutino(message) -> None:
-    get_data(message)
+    get_data(message, token, time_sleep, admin)
     chatIDpessoa=message.chat.id
     bot.reply_to(message, "Aguarde um momento...")
     args = message.text.split()
